b749e3b79ba5b7c5c8c37f13e9285763f3d22daf
Pavithrank02 <pavithrank02@gmail.com>
2025-03-27 15:25:55 +0530

    res

--- src/components/HorizontalMenu.js
+++ src/components/HorizontalMenu.js
@@ -14,12 +14,12 @@ const categories = [
 const HorizontalMenu = () => {
   return (
     <div className="overflow-x-auto scrollbar-hide whitespace-nowrap bg-gradient-to-r from-customBgColor to-customTextColor-light shadow-md p-4 rounded-lg mb-4">
-      <div className="flex space-x-4">
+      <div className="flex flex-wrap gap-4 justify-center sm:justify-start">
         {categories.map((category) => (
           <Link
             key={category.name}
             to={category.path}
-            className="relative px-4 py-2 text-sm font-medium text-customBgColor-bg rounded-full bg-opacity-70 backdrop-blur-lg transition-transform duration-300 ease-in-out transform hover:scale-105 hover:bg-opacity-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-white"
+            className="relative px-4 py-2 text-sm sm:text-base font-medium text-customBgColor-bg rounded-full bg-opacity-70 backdrop-blur-lg transition-transform duration-300 ease-in-out transform hover:scale-105 hover:bg-opacity-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-white"
           >
             <span className="z-10">{category.name}</span>
             <div className="absolute inset-0 rounded-full bg-white opacity-10 transition-opacity duration-300 hover:opacity-20"></div>
